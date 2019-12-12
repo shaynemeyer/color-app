7ac2904b5246bf28530e0bd81ce33126472cd306
Shayne Meyer <shayne.meyer@gmail.com>
2019-12-12 06:57:44 -0800

    Start styling MiniPalette

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
-import MiniPalette from './MiniPalette';
 import './seedColors';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
@@ -12,27 +11,22 @@ function App() {
     return seedColors.find(palette => palette.id === id);
   }
   return (
-    <div>
-      <MiniPalette />
-      <Switch>
-        <Route
-          exact
-          path='/'
-          render={() => <PaletteList palettes={seedColors} />}
-        />
-        <Route
-          exact
-          path='/palette/:id'
-          render={routeParams => (
-            <Palette
-              palette={generatePalette(
-                findPalette(routeParams.match.params.id)
-              )}
-            />
-          )}
-        />
-      </Switch>
-    </div>
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={() => <PaletteList palettes={seedColors} />}
+      />
+      <Route
+        exact
+        path='/palette/:id'
+        render={routeParams => (
+          <Palette
+            palette={generatePalette(findPalette(routeParams.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
 
     // <div>
     //   <Palette palette={generatePalette(seedColors[4])} />
--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,26 +2,45 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-  main: {
-    backgroundColor: 'purple',
-    border: '3px solid teal',
-    '& h1': {
-      color: 'white'
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer'
     }
   },
-  secondary: {
-    backgroundColor: 'pink'
+  colors: {
+    backgroundColor: 'grey'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
   }
 };
 
 function MiniPalette(props) {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
 
-  console.log(classes);
   return (
-    <div className={classes.main}>
-      <h1>Mini Palette</h1>
-      <section className={classes.secondary}>something else</section>
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <div className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </div>
     </div>
   );
 }
