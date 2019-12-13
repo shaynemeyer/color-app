8763dd437895a9b0466388cee2eeceb6b9926eb8
Shayne Meyer <shayne.meyer@gmail.com>
2019-12-12 16:56:33 -0800

    Add Link to ColorBox and Add Route

--- src/App.js
+++ src/App.js
@@ -28,6 +28,11 @@ function App() {
           />
         )}
       />
+      <Route
+        exact
+        path='/palette/:paletteId/colorId'
+        render={() => <h1>SINGLE COLOR PAGE</h1>}
+      />
     </Switch>
 
     // <div>
