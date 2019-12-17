30098d4fb74ada919ddef02c6ff5f0237a24e138
Shayne Meyer <shayne.meyer@gmail.com>
2019-12-17 13:16:41 -0800

    removed useTheme because its not used

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -74,7 +74,6 @@ const useStyles = makeStyles(theme => ({
 
 function NewPaletteForm() {
   const classes = useStyles();
-  const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
