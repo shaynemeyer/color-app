f7227ee6e6bd4e818390eddd519ae5e4843f161f
Shayne Meyer <shayne.meyer@gmail.com>
2019-12-23 13:46:42 -0800

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import { down } from './sizes';
+
 const styles = {
   root: {
     backgroundColor: 'blue',
@@ -11,7 +13,13 @@ const styles = {
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    [down('xl')]: {
+      width: '80%'
+    },
+    [down('xs')]: {
+      width: '75%'
+    }
   },
   nav: {
     display: 'flex',
@@ -29,7 +37,13 @@ const styles = {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '5%',
+    [down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)'
+    }
   }
 };
 
--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -2,7 +2,8 @@ const sizeOptions = {
   xs: '575.98px', // Extra small devices (portrait phones, less than 576px)
   sm: '767.98px', // Small devices (landscape phones, less than 768px)
   md: '991.98px', // Medium devices (tablets, less than 992px)
-  lg: '1199.98px' // Large devices (desktops, less than 1200px)
+  lg: '1199.98px', // Large devices (desktops, less than 1200px),
+  xl: '1600px'
 };
 
 function down(size) {
